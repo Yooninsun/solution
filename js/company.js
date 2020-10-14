a9bd09400a9456a4082175816f03a258e4c4aca5
Yooninsun <jein8954@naver.com>
2020-10-14 12:13:17 +0900

    모바일 화면시 작은 메뉴 변경

--- js/company.js
+++ js/company.js
@@ -10,5 +10,17 @@
         
     })    
     
+    $('.company_bg .mobile_menu > li').on('click', function(e) {
+      e.preventDefault();
+      var url = $(this).attr('href');
+      $('#containerBox').remove();
+        $('#boxArea').load(url);
+        //클릭시 배경색 바꿔주기
+        $(this).parent().addClass('on');
+        $(this).parent().siblings().removeClass('on');
+    })
+
+
+
 
 })(jQuery)
